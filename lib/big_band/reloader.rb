c8643c4c3170bfc1279465fca4655c516c8ff4d7
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-11-04 20:02:27 +0100

    clean up

--- lib/big_band/reloader.rb
+++ lib/big_band/reloader.rb
@@ -95,56 +95,5 @@ class BigBand < Sinatra::Base
       end
     end
 
-=begin
-    def self.registered(klass)
-      klass.register BasicExtensions
-      klass.before do
-        if defined? Thread and Thread.list.size > 1 and Thread.respond_to? :exclusive
-          Thread.exclusive { klass.reload_routes }
-        else
-          klass.reload_routes
-        end
-      end
-    end
-
-    def route(verb, path, options={}, &block)
-      file = caller_files.first.expand_path
-      super.tap do
-        signature = signature.signature if signature.respond_to? :signature
-        routes_from(file) << [verb, signature] if File.exist? file
-      end
-    end
-
-    def reload_index
-      @routes_index ||= {}
-    end
-
-    def routes_from(file)
-      (reload_index[file] ||= [File.mtime(file), []]).last
-    end
-
-    def reload_routes
-      reload_index.each do |file, (mtime, old_routes)|
-        next if mtime >= File.mtime(file) or reload_skip_rules.any? { |r| root_glob(r).include? file }
-        old_routes.each { |verb, signature| (routes[verb] ||= []).delete signature }
-        $LOADED_FEATURES.delete file
-        old_routes.clear
-        require file if File.exist? file
-      end
-    end
-    
-    def reload_skip_rules
-      @reload_skip_rules ||= []
-    end
-    
-    def dont_reload(files)
-      reload_skip_rules << files
-    end
-    
-    def also_reload(files)
-      root_glob(files) { |file| routes_from(file) }
-    end
-=end
-
   end
 end
