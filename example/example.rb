c8eb89b91789ba86b2853ef76a1c7448d067065c
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-11-08 14:00:13 +0100

    reloader is kinda working now, tests to come

--- example/example.rb
+++ example/example.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path(__FILE__ + "/../../lib")
 require "big_band"
 
-class Example < BigBand :except => :Reloader
+class Example < BigBand
 
   # The index page. You should see this comment in YARD.
   get "/" do
