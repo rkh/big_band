7d9ec09c6ff78093c61ede314fd36be8b2c7e178
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-06 23:45:46 +0100

    content_for, css3 transitions

--- lib/big_band/more_helpers.rb
+++ lib/big_band/more_helpers.rb
@@ -2,12 +2,32 @@ require "haml"
 require "big_band/integration" # so this is usable without sinatra
 
 class BigBand < Sinatra::Base
-  
-  # Adds more helper methods (coming soon).
+
+  # Adds more helper methods (more docs coming soon).
   module MoreHelpers
 
     module InstanceMethods
       include Haml::Helpers
+
+      def content_for(name, &block)
+        name = name.to_s
+        @content_for ||= Hash.new {|h,k| h[k] = [] }
+        @content_for[name] << block if block
+        @content_for[name]
+      end
+
+      def yield_content(name, *args)
+        haml_helper do
+          content_for(name).each do |block|
+            result = block.call(*args)
+            haml_concat result unless block_is_haml? block
+          end
+        end
+      end
+
+      def get_content(name, *args)
+        non_haml { yield_content(name, *args) }
+      end
 
       private
 
