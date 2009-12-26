25467ad2f8558259cd6e3a07a9d1a934bc0c6c23
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-26 16:13:49 +0100

    added specs for BigBand::MoreServer

--- spec/big_band/more_server_spec.rb
+++ spec/big_band/more_server_spec.rb
@@ -1,4 +1,7 @@
 require File.expand_path(__FILE__ + "../../../spec_helper.rb")
 
 describe BigBand::MoreServer do
+  before { app :MoreServer }
+  it("should offer unicorn")   { app.server.should include("unicorn")  }
+  it("should offer rainbows")  { app.server.should include("rainbows") }
 end
