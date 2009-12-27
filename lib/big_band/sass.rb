9dc86a03f73872b17f211632cb3f6c7663a72b6b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-28 00:21:48 +0100

    SassScript issue has been fixed (sass repo has a test case for this now, yay)

--- lib/big_band/sass.rb
+++ lib/big_band/sass.rb
@@ -12,13 +12,6 @@ class BigBand < Sinatra::Base
   # This can be used without BigBand or even Sinatra.
   module Sass
     module Functions
-
-      # HACK: Dunno why I have to do both. Have to talk to nex3 or somebody.
-      # Including it just in EvaluationContext will not make the methods available
-      # from Sass, including it just in Functions will cause Sass to raise an
-      # NoMethodError. What causes this behaviour is not obvious from sass/script/functions.rb.
-      # More digging through the Sass code should be done.
-      ::Sass::Script::Functions::EvaluationContext.send :include, self
       ::Sass::Script::Functions.send :include, self
 
       def min(*args)
