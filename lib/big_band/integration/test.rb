018068b0afca745e968542e7d5d438ab39343880
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-27 23:17:50 +0100

    syntactic sugar: added test helpers for defining / browsing routes with dynamic verb assignment

--- lib/big_band/integration/test.rb
+++ lib/big_band/integration/test.rb
@@ -5,24 +5,37 @@ require "big_band"
 require "big_band/integration"
 
 require "rack/test"
-require "webrat"
-Webrat.configure { |config| config.mode = :sinatra }
+#require "webrat"
+#Webrat.configure { |config| config.mode = :sinatra }
 
 module BigBand::Integration
   module Test
 
-    include Webrat::Methods
+    #include Webrat::Methods
     include Rack::Test::Methods
-    
+
     attr_writer :app
     def app(*options, &block)
       unless block.nil? and options.empty?
-        superclass = options.first if options.size == 1 and options.first.is_a? Class
+        superclass   = options.first if options.size == 1 and options.first.is_a? Class
         superclass ||= BigBand(*options)
-        @app = Class.new(superclass, &block)
+        @app         = Class.new(superclass, &block)
+        inspection   = "app"
+        inspection << "(" << options.map { |o| o.inspect }.join(", ") << ")" unless options.empty?
+        inspection << " { ... }" if block
+        @app.class_eval "def self.inspect; #{inspection.inspect}; end"
       end
       @app || BigBand.applications.last || Sinatra::Application
     end
-    
+
+    def define_route(verb, *args, &block)
+      app.send(verb, *args, &block)
+    end
+
+    def browse_route(verb, *args, &block)
+      send(verb, *args, &block)
+      last_response
+    end
+
   end
 end
