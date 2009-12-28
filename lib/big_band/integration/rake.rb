40c78a89c48dc7ae9fb7fbc3e82f60d5a02ca60f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-28 12:34:41 +0100

    some rake documentation

--- lib/big_band/integration/rake.rb
+++ lib/big_band/integration/rake.rb
@@ -3,7 +3,40 @@ require "rake"
 require "rake/tasklib"
 
 module BigBand::Integration
-  # Some Rake example and description goes here.
+  # In your Rakefile, do the following:
+  #
+  #   require "big_band/integration/rake"
+  #   include BigBand::Integration::Rake
+  #
+  #   RoutesTask.new
+  #
+  # then you can run 'rake routes' from your
+  # project directory and it will list all routes
+  # of your app. Per default it will scan for routes
+  # defined in ruby files in the directories lib, app,
+  # routes, models, views, and controllers (ignoring
+  # non-existant directories, of course). You can change
+  # that behaviour by setting +source+ to another pattern:
+  #
+  #   RoutesTask.new { |t| t.source = "**/*.rb" }
+  #
+  # However, you may also just pass in a Sinatra app, so it
+  # will not have to scan through the source files:
+  #
+  #   require "my_app"
+  #   RoutesTask.new { |t| t.source = MyApp }
+  #
+  # Keep in mind that broken my_app in that case will also make
+  # your Rakefile unusable.
+  #
+  # Also, you may set another name for the task either by setting
+  # the first argument or defining calling #name=:
+  #
+  #   RoutesTask.new(:some_routes) { |t| t.souce = SomeApp }
+  #   RoutesTask.new do |t|
+  #     t.source = AnotherApp
+  #     t.name   = :other_routes
+  #   end
   module Rake
     class RoutesTask < ::Rake::TaskLib
       attr_accessor :source, :name
