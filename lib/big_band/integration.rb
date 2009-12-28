3f8ef72d7822b8493cf048a53837f56dbad8f397
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-28 16:00:42 +0100

    Store list of parsed routes properly, offer helper for looping through all routes.

--- lib/big_band/integration.rb
+++ lib/big_band/integration.rb
@@ -15,12 +15,26 @@ class BigBand < Sinatra::Base
         source.routes
       when String
         require "big_band/integration/yard"
-        YARD::Registry.load(Dir[files], true)
+        ::YARD::Registry.load(Dir[source], true)
         YARD::Handlers::Sinatra::AbstractRouteHandler.routes.inject({}) do |routes, route_object|
-          routes.merge route_object.http_verb => route_object.http_path
+          routes[route_object.http_verb] ||= []
+          routes[route_object.http_verb] << route_object.http_path
+          routes
         end
       else
         raise ArgumentError, "cannot retrieve routes for #{source.inspect}"
+      end
+    end
+
+    def self.each_route(source)
+      routes_for(source).each do |verb, routes|
+        routes.each do |route|
+          path   = route.path    if route.respond_to? :path
+          path ||= route.pattern if route.respond_to? :pattern
+          path ||= route[0]      if route.is_a? Array
+          path ||= route
+          yield(verb, path)
+        end
       end
     end
 
