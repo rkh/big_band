46ae66d59eb48161cca48a324d791a325838c534
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-12-30 12:21:52 +0100

    check just if method is directly defined in class, not in ancestors (thus we may set server and akin).

--- lib/big_band/config_file.rb
+++ lib/big_band/config_file.rb
@@ -39,7 +39,7 @@ class BigBand < Sinatra::Base
     def config_file(*paths)
       paths.each do |pattern|
         files = root_glob(pattern.to_s)
-        files.each { |f| YAML.load_file(f).each_pair { |k,v| set k, v unless respond_to? k } }
+        files.each { |f| YAML.load_file(f).each_pair { |k,v| set k, v unless methods(false).any? { |m| m.to_s = k.to_s } } }
         warn "WARNING: could not load config file #{pattern}" if files.empty?
       end
     end
