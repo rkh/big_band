b37c160d0972a2062361f440e910786860977fed
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-19 14:34:05 +0100

    add rspec as dependency, so it'll work on a clean system

--- dependencies.rb
+++ dependencies.rb
@@ -5,6 +5,7 @@ dependencies = proc do
   dep "monkey-lib", :git => "git://github.com/rkh/monkey-lib.git",        :version => "0.3.5",  :git_ref => "v%s"
   dep "rack",       :git => "git://github.com/rack/rack.git",             :version => "1.0.1",  :only    => :rip
   dep "rack-test",  :git => "git://github.com/brynary/rack-test.git",     :version => "0.5.3",  :git_ref => "v%s"
+  dep "rspec",      :git => "git://github.com/dchelimsky/rspec.git",      :version => "1.3.0"
   dep "sinatra",    :git => "git://github.com/sinatra/sinatra.git",       :version => "0.9.4"
   dep "yard",       :git => "git://github.com/lsegal/yard.git",           :version => "0.5.2"
 end
