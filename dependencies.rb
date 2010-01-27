5d1587c84bfeed90b6d096a0dac00b5064a38c57
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-01-27 14:50:47 +0100

    monkey-lib version

--- dependencies.rb
+++ dependencies.rb
@@ -3,7 +3,7 @@ require "depgen/depgen"
 class Dependencies < Depgen
   add "compass",    :git => "git://github.com/chriseppstein/compass.git", :version => "0.8.17",  :git_ref => "v%s"
   add "haml",       :git => "git://github.com/nex3/haml.git",             :version => "2.2.17",  :only    => :rip
-  add "monkey-lib", :git => "git://github.com/rkh/monkey-lib.git",        :version => "0.3.5.1", :git_ref => "v%s"
+  add "monkey-lib", :git => "git://github.com/rkh/monkey-lib.git",        :version => "0.3.5.4", :git_ref => "v%s"
   add "rack",       :git => "git://github.com/rack/rack.git",             :version => "1.0.1",   :only    => :rip
   add "rack-test",  :git => "git://github.com/brynary/rack-test.git",     :version => "0.5.3",   :git_ref => "v%s"
   add "rspec",      :git => "git://github.com/dchelimsky/rspec.git",      :version => "1.3.0"
