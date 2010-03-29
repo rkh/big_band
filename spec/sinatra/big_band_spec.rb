43f98a1786c37fbbdc70c5ac570989bca764f642
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-03-29 21:17:53 +0200

    adjust specs

--- spec/sinatra/big_band_spec.rb
+++ spec/sinatra/big_band_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path(__FILE__ + "/../../spec_helper.rb")
 describe Sinatra::BigBand do
   before { @example_app = nil }
 
-  def set_example_app(*options)
-    @example_app = Class.new Sinatra::BigBand(*options)
+  def set_example_app(options = {})
+    @example_app = Class.new Sinatra::BigBand(options)
   end
 
   def example_app
@@ -15,20 +15,9 @@ describe Sinatra::BigBand do
     [:AdvancedRoutes, :Compass, :ConfigFile, :MoreServer, :Namespace, :Sugar].each do |ext_name|
       describe ext_name do
         before { @ext_name = ext_name }
-
-        def extension
-          extension = Sinatra.const_get @ext_name
-        end
-
-        it "should be loaded" do
-          pending
-          Sinatra.should be_const_defined(ext_name)
-        end
-
-        it "should be set up" do
-          pending
-          example_app.should be_a(extension)
-        end
+        def extension; Sinatra.const_get @ext_name end
+        it("should be loaded") { Sinatra.should be_const_defined(ext_name) }
+        it("should be set up") { example_app.should be_a(extension) }
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,2 @@
 require "sinatra/rspec"
 require "sinatra/big_band"
-
-Sinatra::BigBand::Subproject.generate_meta_data!
